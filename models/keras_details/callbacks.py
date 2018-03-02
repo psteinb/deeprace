8cce3bc766a21e242a8ffc2657875b02f44538ae
Peter Steinbach <steinbac@mpi-cbg.de>
2018-03-02 17:39:11 +0100

    fixed timings

--- models/keras_details/callbacks.py
+++ models/keras_details/callbacks.py
@@ -7,12 +7,14 @@ class stopwatch(keras.callbacks.Callback):
         self.train_begin = dt.datetime.now()
         self.epoch_durations = []
         self.epoch_start = []
+        self.epoch_start_dt = []
 
     def on_epoch_begin(self, batch, logs={}):
-        self.epoch_start.append(dt.datetime.now())
+        self.epoch_start_dt.append(dt.datetime.now())
+        self.epoch_start.append((dt.datetime.now() - self.train_begin).total_seconds())
 
     def on_epoch_end(self, batch, logs={}):
-        self.epoch_durations.append((dt.datetime.now() - self.epoch_start[-1]).total_seconds())
+        self.epoch_durations.append((dt.datetime.now() - self.epoch_start_dt[-1]).total_seconds())
 
     def on_train_end(self, logs={}):
         self.train_end = dt.datetime.now()
