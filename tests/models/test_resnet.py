fd42066458cb6645d5ed8b82e8454205c1b5244d
Peter Steinbach <steinbac@mpi-cbg.de>
2018-03-26 13:26:31 +0200

    finished test on model name parser

--- tests/models/test_resnet.py
+++ tests/models/test_resnet.py
@@ -23,3 +23,29 @@ def test_resnet50v2():
 
     assert res['n'] == 5
     assert res['version'] == 2
+
+
+def test_resnet56v1_string():
+
+    tstr = "resnet56v1"
+    res = params_from_name(tstr)
+
+    assert res['n'] == 9
+    assert res['version'] == 1
+
+def test_resnet56v2_string():
+
+    tstr = "resnet56v2"
+    res = params_from_name(tstr)
+
+    assert res['n'] == 6
+    assert res['version'] == 2
+
+
+def test_resnet32v1_string():
+
+    tstr = "resnet32v1"
+    res = params_from_name(tstr)
+
+    assert res['n'] == 5
+    assert res['version'] == 1
