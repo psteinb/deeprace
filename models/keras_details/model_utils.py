ae6736b8eed45980ec28ee7717087f8dca519b44
Peter Steinbach <steinbac@mpi-cbg.de>
2018-03-26 14:46:58 +0200

    added function to calculate number of parameters per model

---
+++ models/keras_details/model_utils.py
@@ -0,0 +1,12 @@
+import keras.backend as K
+from keras.models import Model
+
+import numpy as np
+
+def model_size(inmodel):
+    """Compute number of params in a inmodel (the actual number of floats)"""
+    #stolen from https://stackoverflow.com/questions/35792278/how-to-find-number-of-parameters-of-a-keras-inmodel/35827171
+    weights = inmodel.get_weights()
+    print(">> ",type(weights),type(weights[0]))
+    return sum([np.prod(# K.get_value
+                        (w).shape) for w in weights])
