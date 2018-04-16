70af56700ec787de5bbeaceceaa8f6a29158da34
Peter Steinbach <steinbac@mpi-cbg.de>
2018-04-16 15:57:14 +0200

    added scripts for rocm based tensorflow

---
+++ scripts/rocm-short/resnet50.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd /deeprace/
+
+export HIP_VISIBLE_DEVICES=3
+
+for i in 32 64 128 256 512;
+do
+	 echo
+	 echo $i
+	 echo
+	 for r in `seq 1 10`;
+	 do
+	     echo "$0 batch_size=$i run ${r}/10"
+	     python3 ./deeprace.py train -c "docker,mi25:1,batch_size=${i}" -t /deeprace/scripts/rocm-short/resnet56v1_bs${i}_rocm_0.tsv -e 15 resnet56v1 > ./scripts/rocm-short/resnet56v1_bs${i}_rocm_${r}.log 2>&1
+	 done
+done
