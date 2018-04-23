e563108203c5a9bf6a5ef20327611bfe4af88bb6
Peter Steinbach <steinbach@scionics.de>
2018-04-23 13:51:00 +0000

    added safe-guard for pre-emptive loss of instance on gce

--- scripts/gce/resnet30.sh
+++ scripts/gce/resnet30.sh
@@ -8,8 +8,12 @@ do
 	 echo
      for i in 512 265 128 64 32;
 	 do
-	     echo "$0 batch_size=$i run ${r}/10"
-         singularity exec -B /home/steinbac/deeprace:/deeprace --nv /home/steinbac/tf1.3-plus.simg python /deeprace/deeprace.py train -O batch_size=${i} -c "k80:1,fs:gce,singularity:gce" -t /home/steinbac/resnet32v1-short-gce-bs${i}-singularity_${r}.tsv -e 10 resnet32v1
-
+	     if [[ -e /home/steinbac/resnet32v1-short-gce-bs${i}-singularity_${r}.tsv ]];then
+             echo "$0 batch_size=$i run ${r}/10 already exists"
+             continue;
+         else
+             echo "$0 batch_size=$i run ${r}/10"
+             singularity exec -B /home/steinbac/deeprace:/deeprace --nv /home/steinbac/tf1.3-plus.simg python /deeprace/deeprace.py train -O batch_size=${i} -c "k80:1,fs:gce,singularity:gce" -t /home/steinbac/resnet32v1-short-gce-bs${i}-singularity_${r}.tsv -e 10 resnet32v1
+         fi
 	 done
 done
