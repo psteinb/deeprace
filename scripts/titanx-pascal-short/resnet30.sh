b3f0935342681a2c248c128ddead83c7360ba320
Peter Steinbach <steinbac@mpi-cbg.de>
2018-04-23 14:07:59 +0200

    modified folder names according to gpu model

--- scripts/titanx-pascal-short/resnet30.sh
+++ scripts/titanx-pascal-short/resnet30.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /home/steinba/development/deeprace/scripts/gtx1080-short
+cd /home/steinba/development/deeprace/scripts/titanx-pascal-short
 export CUDA_VISIBLE_DEVICES=1
 
 module load singularity/2.4.2
@@ -13,9 +13,9 @@ do
 	 for i in 32 64 128 256 512;
 	 do
 	     echo "$0 batch_size=$i run ${r}/10"
-	     singularity exec -B /home/steinbac/development/deeprace/:/deeprace --nv /projects/steinbac/software/singularity/sandbox//tf1.3-plus.simg python3 /deeprace/deeprace.py train -O batch_size=${i} -c "gtx1080:1,fs:nfs,singularity:lustre" -t /deeprace/scripts/gtx1080-short/resnet32v1-short-bs${i}-singularity-${r}.tsv -e 10 resnet32v1
+	     singularity exec -B /home/steinbac/development/deeprace/:/deeprace --nv /projects/steinbac/software/singularity/sandbox//tf1.3-plus.simg python3 /deeprace/deeprace.py train -O batch_size=${i} -c "titanx-pascal:1,fs:nfs,singularity:lustre" -t /deeprace/scripts/titanx-pascal-short/resnet32v1-short-bs${i}-singularity-${r}.tsv -e 10 resnet32v1
 
-	     singularity exec -B /home/steinbac/development/deeprace/:/deeprace --nv /projects/steinbac/software/singularity/sandbox//tf1.7-plus.simg python3 /deeprace/deeprace.py train -O batch_size=${i} -b tf -c "gtx1080:1,fs:nfs,singularity:lustre" -t /deeprace/scripts/gtx1080-short/resnet32v1-short-bs${i}-singularity-${r}.tsv -e 10 resnet32v1
+	     singularity exec -B /home/steinbac/development/deeprace/:/deeprace --nv /projects/steinbac/software/singularity/sandbox//tf1.7-plus.simg python3 /deeprace/deeprace.py train -O batch_size=${i} -b tf -c "titanx-pascal:1,fs:nfs,singularity:lustre" -t /deeprace/scripts/titanx-pascal-short/resnet32v1-short-bs${i}-singularity-${r}.tsv -e 10 resnet32v1
 
 	 done
 done
@@ -30,9 +30,9 @@ do
 	 for i in 32 64 128 256 512;
 	 do
 	     echo "$0 batch_size=$i run ${r}/10"
-	     singularity exec -B /home/steinbac/development/deeprace/:/deeprace --nv /projects/steinbac/software/singularity/sandbox//tf1.3-plus.simg python3 /deeprace/deeprace.py train -O batch_size=${i},n_gpus=2 -c "gtx1080:2,fs:nfs,singularity:lustre" -t /deeprace/scripts/gtx1080-short/resnet32v1-short-bs${i}-2gpus-singularity-${r}.tsv -e 10 resnet32v1
+	     singularity exec -B /home/steinbac/development/deeprace/:/deeprace --nv /projects/steinbac/software/singularity/sandbox//tf1.3-plus.simg python3 /deeprace/deeprace.py train -O batch_size=${i},n_gpus=2 -c "titanx-pascal:2,fs:nfs,singularity:lustre" -t /deeprace/scripts/titanx-pascal-short/resnet32v1-short-bs${i}-2gpus-singularity-${r}.tsv -e 10 resnet32v1
 
-	     singularity exec -B /home/steinbac/development/deeprace/:/deeprace --nv /projects/steinbac/software/singularity/sandbox//tf1.7-plus.simg python3 /deeprace/deeprace.py train -O batch_size=${i},n_gpus=2 -b tf -c "gtx1080:2,fs:nfs,singularity:lustre" -t /deeprace/scripts/gtx1080-short/resnet32v1-short-bs${i}-2gpus-singularity-${r}.tsv -e 10 resnet32v1
+	     singularity exec -B /home/steinbac/development/deeprace/:/deeprace --nv /projects/steinbac/software/singularity/sandbox//tf1.7-plus.simg python3 /deeprace/deeprace.py train -O batch_size=${i},n_gpus=2 -b tf -c "titanx-pascal:2,fs:nfs,singularity:lustre" -t /deeprace/scripts/titanx-pascal-short/resnet32v1-short-bs${i}-2gpus-singularity-${r}.tsv -e 10 resnet32v1
 
 	 done
 done
