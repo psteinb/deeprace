94878fa2350e4f2787916d9c37ec174b05b1a63d
Peter Steinbach <steinbac@mpi-cbg.de>
2018-05-09 14:20:20 +0200

    fixed problem with specifying the dataset

--- models/keras_details/care_denoise2d_details.py
+++ models/keras_details/care_denoise2d_details.py
@@ -79,10 +79,15 @@ def train(train, test, datafraction, optsdict):
     logging.info("%s (%i epochs):: batch_size = %i, depth = %i, checkpoint %i", model_type, epochs,batch_size,depth,checkpoint_epochs)
 
     nsamples_train = int(math.floor(train[0].shape[0]*datafraction))
-
+    nsamples_train = int(math.floor(train[0].shape[0]*datafraction))
 
     x_train, y_train = train[0],train[-1]
-    logging.info("received training data shapes x=%s y=%s" % (x_train.shape, y_train.shape))
+
+    if datafraction != 1.0:
+        x_train = train[0][:nsamples_train,]
+        y_train = train[-1][:nsamples_train,]
+
+    logging.info("using x=%s y=%s for training (original: x=%s y=%s) " % (x_train.shape, y_train.shape,train[0].shape, train[1].shape))
 
     # Input image dimensions.
     #input_shape = x_train.shape[1:]
