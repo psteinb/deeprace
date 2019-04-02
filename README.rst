94d8893807183d232a9522a7f31ca4d2121bb414
Peter Steinbach <steinbac@mpi-cbg.de>
2019-04-02 13:05:03 +0200

    updated setup for building and tox

--- README.rst
+++ README.rst
@@ -1,30 +1,101 @@
-===============================
-deeprace
-===============================
+========
+Overview
+========
 
-.. image:: https://badge.fury.io/py/deeprace.png
-    :target: http://badge.fury.io/py/deeprace
+.. start-badges
 
-.. image:: https://travis-ci.org/psteinb/deeprace.png?branch=master
-        :target: https://travis-ci.org/psteinb/deeprace
+.. list-table::
+    :stub-columns: 1
 
-.. image:: https://pypip.in/d/deeprace/badge.png
-        :target: https://crate.io/packages/deeprace?version=latest
+    * - docs
+      - |docs|
+    * - tests
+      - | |travis| |appveyor| |requires|
+        | |codecov|
+    * - package
+      - | |version| |wheel| |supported-versions| |supported-implementations|
+        | |commits-since|
+.. |docs| image:: https://readthedocs.org/projects/deeprace/badge/?style=flat
+    :target: https://readthedocs.org/projects/deeprace
+    :alt: Documentation Status
+
+.. |travis| image:: https://travis-ci.org/psteinb/deeprace.svg?branch=master
+    :alt: Travis-CI Build Status
+    :target: https://travis-ci.org/psteinb/deeprace
+
+.. |appveyor| image:: https://ci.appveyor.com/api/projects/status/github/psteinb/deeprace?branch=master&svg=true
+    :alt: AppVeyor Build Status
+    :target: https://ci.appveyor.com/project/psteinb/deeprace
+
+.. |requires| image:: https://requires.io/github/psteinb/deeprace/requirements.svg?branch=master
+    :alt: Requirements Status
+    :target: https://requires.io/github/psteinb/deeprace/requirements/?branch=master
+
+.. |codecov| image:: https://codecov.io/github/psteinb/deeprace/coverage.svg?branch=master
+    :alt: Coverage Status
+    :target: https://codecov.io/github/psteinb/deeprace
+
+.. |version| image:: https://img.shields.io/pypi/v/deeprace.svg
+    :alt: PyPI Package latest release
+    :target: https://pypi.org/project/deeprace
+
+.. |commits-since| image:: https://img.shields.io/github/commits-since/psteinb/deeprace/v0.0.0.svg
+    :alt: Commits since latest release
+    :target: https://github.com/psteinb/deeprace/compare/v0.0.0...master
+
+.. |wheel| image:: https://img.shields.io/pypi/wheel/deeprace.svg
+    :alt: PyPI Wheel
+    :target: https://pypi.org/project/deeprace
+
+.. |supported-versions| image:: https://img.shields.io/pypi/pyversions/deeprace.svg
+    :alt: Supported versions
+    :target: https://pypi.org/project/deeprace
+
+.. |supported-implementations| image:: https://img.shields.io/pypi/implementation/deeprace.svg
+    :alt: Supported implementations
+    :target: https://pypi.org/project/deeprace
 
 
-benchmark suite to time deep learning training in a framework agnostic fashion
+.. end-badges
 
-Features
---------
+simple benchmark suite for ensemble based deep learning benchmarking
 
-* TODO
+* Free software: BSD 3-Clause License
 
-Requirements
-------------
+Installation
+============
 
-- Python >= 2.6 or >= 3.3
+::
 
-License
--------
+    pip install deeprace
 
-MIT licensed. See the bundled `LICENSE <https://github.com/psteinb/deeprace/blob/master/LICENSE>`_ file for more details.
+Documentation
+=============
+
+
+https://deeprace.readthedocs.io/
+
+
+Development
+===========
+
+To run the all tests run::
+
+    tox
+
+Note, to combine the coverage data from all the tox environments run:
+
+.. list-table::
+    :widths: 10 90
+    :stub-columns: 1
+
+    - - Windows
+      - ::
+
+            set PYTEST_ADDOPTS=--cov-append
+            tox
+
+    - - Other
+      - ::
+
+            PYTEST_ADDOPTS=--cov-append tox
--- setup.py
+++ setup.py
@@ -1,85 +1,88 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import print_function
+
+import io
 import re
-import sys
+from glob import glob
+from os.path import basename
+from os.path import dirname
+from os.path import join
+from os.path import splitext
+
+from setuptools import find_packages
 from setuptools import setup
-from setuptools.command.test import test as TestCommand
-import versioneer
-
-REQUIRES = [
-    'docopt','snakemake','versioneer'
-]
-
-class PyTest(TestCommand):
-    user_options = [('pytest-args=', 'a', "Arguments to pass to pytest")]
-
-    def initialize_options(self):
-        TestCommand.initialize_options(self)
-        self.pytest_args = ''
-
-    def run_tests(self):
-        import shlex
-        #import here, cause outside the eggs aren't loaded
-        import pytest
-        errno = pytest.main(shlex.split(self.pytest_args))
-        sys.exit(errno)
 
 
-def find_version(fname):
-    '''Attempts to find the version number in the file names fname.
-    Raises RuntimeError if not found.
-    '''
-    version = ''
-    with open(fname, 'r') as fp:
-        reg = re.compile(r'__version__ = [\'"]([^\'"]*)[\'"]')
-        for line in fp:
-            m = reg.match(line)
-            if m:
-                version = m.group(1)
-                break
-    if not version:
-        raise RuntimeError('Cannot find version information')
-    return version
+def read(*names, **kwargs):
+    with io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ) as fh:
+        return fh.read()
 
-__version__ = find_version("deeprace.py")
-
-
-def read(fname):
-    with open(fname) as fp:
-        content = fp.read()
-    return content
 
 setup(
     name='deeprace',
-    version=versioneer.get_version(),
-    description='benchmark suite to time deep learning training in a framework agnostic fashion',
-    long_description=read("README.rst"),
+    version='0.0.0',
+    license='BSD 3-Clause License',
+    description='simple benchmark suite for ensemble based deep learning benchmarking',
+    long_description='%s\n%s' % (
+        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
+        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    ),
     author='Peter Steinbach',
     author_email='steinbach@scionics.de',
     url='https://github.com/psteinb/deeprace',
-    install_requires=REQUIRES,
-    license=read("LICENSE"),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
     zip_safe=False,
-    keywords='deeprace',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        # "Programming Language :: Python :: 2",
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: Unix',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        # 'Programming Language :: Python',
         # 'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        'Programming Language :: Python :: Implementation :: PyPy',
+        # uncomment if you test on these interpreters:
+        # 'Programming Language :: Python :: Implementation :: IronPython',
+        # 'Programming Language :: Python :: Implementation :: Jython',
+        # 'Programming Language :: Python :: Implementation :: Stackless',
+        'Topic :: Utilities',
     ],
-    py_modules=["deeprace", "models", "datasets", "verbs"],
+    project_urls={
+        'Documentation': 'https://deeprace.readthedocs.io/',
+        'Changelog': 'https://deeprace.readthedocs.io/en/latest/changelog.html',
+        'Issue Tracker': 'https://github.com/psteinb/deeprace/issues',
+    },
+    keywords=[
+        # eg: 'keyword1', 'keyword2', 'keyword3',
+    ],
+    python_requires='>=3.4, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*',
+    install_requires=[
+        'docopt==0.6.2', 'invoke==0.14.0', 'numpy', 'tifffile==0.15.1', 'keras==2.2.4', 'tensorflow>=1.7.0'
+    ],
+    extras_require={
+        # eg:
+        #   'rst': ['docutils>=0.11'],
+        #   ':python_version=="2.6"': ['argparse'],
+    },
     entry_points={
         'console_scripts': [
-            "deeprace = deeprace:main"
+            'deeprace = deeprace.cli:main',
         ]
     },
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest, 'version' : versioneer.get_cmdclass()}
 )
